fe2ac7e2b28d89c7b665dee0966338077fcc153c
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-02-27 09:06:59 +0100

    ensure ip range end is always greater or equal to beginning

--- src/ip_range.rs
+++ src/ip_range.rs
@@ -1,8 +1,61 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 use std::net::Ipv4Addr;
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Clone, Debug)]
 pub struct IpRange {
     pub start: Ipv4Addr,
     pub end: Ipv4Addr,
+}
+
+impl<'de> Deserialize<'de> for IpRange {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        struct IpRangeHelper {
+            start: Ipv4Addr,
+            end: Ipv4Addr,
+        }
+
+        let helper = IpRangeHelper::deserialize(deserializer)?;
+
+        if helper.start <= helper.end {
+            Ok(IpRange {
+                start: helper.start,
+                end: helper.end,
+            })
+        } else {
+            Err(serde::de::Error::custom(
+                "end IP must be greater than or equal to start IP",
+            ))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deserialize() {
+        let res = toml::from_str::<IpRange>(
+            r#"start = "10.128.0.2"
+            end = "10.128.0.10""#,
+        );
+        assert!(res.is_ok());
+        let range = res.unwrap();
+        assert!(range.start == Ipv4Addr::new(10, 128, 0, 2));
+        assert!(range.end == Ipv4Addr::new(10, 128, 0, 10));
+        let res2 = toml::from_str::<IpRange>(
+            r#"start = "10.128.0.2"
+            end = "10.128.0.1""#,
+        );
+        assert!(res2.is_err());
+        let res3 = toml::from_str::<IpRange>(
+            r#"start = "10.128.0.2"
+            end = "10.12.0.10""#,
+        );
+        assert!(res2.is_err());
+    }
 }
