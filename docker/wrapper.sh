1f8632bc38130c2fbafb51fae77c4fd567262e0e
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-04-02 16:20:22 +0200

    fix formatting

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -8,7 +8,7 @@ if [ -z "$key" ]; then
   exit 1
 fi
 
-awk -v key="$key" '{gsub(/PrivateKey = <private key>/, "PrivateKey = " key); print}' wggvpn.conf > temp.conf && mv temp.conf wggvpn.conf
+awk -v key="$key" '{gsub(/PrivateKey = <private key>/, "PrivateKey = " key); print}' wggvpn.conf >temp.conf && mv temp.conf wggvpn.conf
 
 chmod 600 wggvpn.conf
 ./gnosis_vpn-server --config-file ./config.toml serve --periodically-run-cleanup --sync-wg-interface
