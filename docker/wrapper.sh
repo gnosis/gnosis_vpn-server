25fc032c082047d5f10298a241edd3484a918f01
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2025-08-19 19:25:54 +0200

    simple squid config

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -11,5 +11,5 @@ fi
 awk -v key="$key" '{gsub(/PrivateKey = <private key>/, "PrivateKey = " key); print}' wggvpn.conf >temp.conf && mv temp.conf wggvpn.conf
 
 chmod 600 wggvpn.conf
-squid
+squid -f ./squid.conf
 ./gnosis_vpn-server --config-file ./config.toml serve --periodically-run-cleanup --sync-wg-interface
