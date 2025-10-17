4ae54fc55b5b7c2859f1afae2a7997722bdc219c
ausias-armesto <ausiasarmesto@gmail.com>
2025-10-17 15:08:46 +0200

    Unify setup

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -2,13 +2,9 @@
 
 set -o errexit
 
-declare key="$WG_PRIVATE_KEY"
-if [ -z "$key" ]; then
+if [ ! -f /app/privatekey ]; then
   echo "WG_PRIVATE_KEY is not set - generating a new one"
-  key=$(wg genkey)
+  wg genkey > /app/privatekey
 fi
 
-sed -i "s/PrivateKey = <private key>/PrivateKey = ${WG_PRIVATE_KEY}/" wggvpn.conf
-
-chmod 600 wggvpn.conf
 ./gnosis_vpn-server --config-file ./config.toml serve --periodically-run-cleanup --sync-wg-interface
