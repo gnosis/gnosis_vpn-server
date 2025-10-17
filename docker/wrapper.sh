1455233f1848fcea894049d25ab84b7e69100459
ausias-armesto <ausiasarmesto@gmail.com>
2025-10-17 15:22:11 +0200

    Fix lint

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 if [ ! -f /app/privatekey ]; then
   echo "WG_PRIVATE_KEY is not set - generating a new one"
-  wg genkey > /app/privatekey
+  wg genkey >/app/privatekey
 fi
 
 ./gnosis_vpn-server --config-file ./config.toml serve --periodically-run-cleanup --sync-wg-interface
