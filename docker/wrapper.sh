56c082025c4c14a20bf47cd74c97787f80612506
Ausiàs Armesto <ausiasarmesto@gmail.com>
2026-04-15 08:50:02 +0200

    Fixing docker registry

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -11,5 +11,4 @@ fi
 awk -v key="$key" '{gsub(/PrivateKey = <private key>/, "PrivateKey = " key); print}' wggvpn.conf >temp.conf && mv temp.conf wggvpn.conf
 
 chmod 600 wggvpn.conf
-squid -f ./squid.conf
 ./gnosis_vpn-server --config-file ./config.toml serve --periodically-run-cleanup --sync-wg-interface
